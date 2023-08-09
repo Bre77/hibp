f1f731a440e214ac4fc8285954c1d6fa123c5881
Brett Adams <brett.whynot@gmail.com>
2023-08-09 01:43:37 +0000

    Add pip install

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,4 @@
+/opt/splunk/bin/splunk cmd python3 -m pip install --upgrade -t src/main/resources/splunk/lib -r src/main/resources/splunk/lib/requirements.txt --no-dependencies
 yarn install
 yarn run build
 yarn run link:app
