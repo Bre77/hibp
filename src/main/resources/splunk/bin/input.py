12a33119058d7b78dd64b27670e9f2b1e1ae4744
Brett Adams <brett.whynot@gmail.com>
2023-10-14 02:23:17 +0000

    Move reset endpoint to input endpoint

--- src/main/resources/splunk/bin/input.py
+++ src/main/resources/splunk/bin/input.py
@@ -15,63 +15,93 @@ class index(PersistentServerConnectionApplication):
 
     def handle(self, in_string):
         args = json.loads(in_string)
-        if args["method"] != "POST":
-            return {"payload": "", "status": 504}
-
-        form = dict(args.get("form", []))
         LOCAL_URI = args["server"]["rest_uri"]
         AUTHTOKEN = args["session"]["authtoken"]
-        self.logger.info(LOCAL_URI)
 
-        try:
-            INDEX = form["index"]
-        except KeyError:
-            return {"payload": "No index provided", "status": 400}
-        
+        if args["method"] == "POST":
+            form = dict(args.get("form", []))
 
-        try:
-            if(INDEX is not ""):
-                self.logger.info("HIBP Input Manager: Setting index")
+            try:
+                INDEX = form["index"]
+            except KeyError:
+                return {"payload": "No index provided", "status": 400}
+            
+
+            try:
+                if(INDEX is not ""):
+                    self.logger.info("HIBP Input Manager: Setting index")
+                    simpleRequest(
+                        f"{LOCAL_URI}/servicesNS/nobody/{self.APP_NAME}/properties/inputs/hibp_domainsearch%3A%2F%2Fdefault",
+                        sessionKey=AUTHTOKEN,
+                        method="POST",
+                        postargs={"index": INDEX},
+                        raiseAllErrors=True,
+                    )
+                    self.logger.info("HIBP Input Manager: Enabling input")
+                    simpleRequest(
+                        f"{LOCAL_URI}/servicesNS/nobody/{self.APP_NAME}/data/inputs/hibp_domainsearch/default/enable",
+                        sessionKey=AUTHTOKEN,
+                        method="POST",
+                        raiseAllErrors=True,
+                    )
+                    self.logger.info("HIBP Input Manager: Updating macro")
+                    simpleRequest(
+                        f"{LOCAL_URI}/servicesNS/nobody/{self.APP_NAME}/configs/conf-macros/hibp_index",
+                        sessionKey=AUTHTOKEN,
+                        method="POST",
+                        postargs={"definition": f"index={INDEX}"},
+                        raiseAllErrors=False,
+                    )
+                    self.logger.info("HIBP Input Manager: Marking as configured")
+                    simpleRequest(
+                        f"{LOCAL_URI}/servicesNS/nobody/{self.APP_NAME}/apps/local/hibp",
+                        sessionKey=AUTHTOKEN,
+                        method="POST",
+                        postargs={"configured": "1"},
+                        raiseAllErrors=False,
+                    )
+                else:
+                    self.logger.info("HIBP Input Manager: Disabling input")
+                    simpleRequest(
+                        f"{LOCAL_URI}/servicesNS/nobody/{self.APP_NAME}/data/inputs/hibp_domainsearch/default/disable",
+                        sessionKey=AUTHTOKEN,
+                        method="POST",
+                        raiseAllErrors=True,
+                    )
+
+                return {"payload": "", "status": 200}
+            except Exception as e:
+                return {"payload": str(e), "status": 500}
+            
+        elif args['method'] == "DELETE":
+            try:
+                # Remove all entries from hibp-pwned collection
+                self.logger.info("HIBP Reset: Removing all entries from hibp-pwned")
                 simpleRequest(
-                    f"{LOCAL_URI}/servicesNS/nobody/{self.APP_NAME}/properties/inputs/hibp_domainsearch%3A%2F%2Fdefault",
+                    f"{LOCAL_URI}/servicesNS/nobody/{self.APP_NAME}/collections/data/hibp-pwned",
                     sessionKey=AUTHTOKEN,
-                    method="POST",
-                    postargs={"index": INDEX},
+                    method="DELETE",
                     raiseAllErrors=True,
                 )
-                self.logger.info("HIBP Input Manager: Enabling input")
-                simpleRequest(
-                    f"{LOCAL_URI}/servicesNS/nobody/{self.APP_NAME}/data/inputs/hibp_domainsearch/default/enable",
-                    sessionKey=AUTHTOKEN,
-                    method="POST",
-                    raiseAllErrors=True,
-                )
-                self.logger.info("HIBP Input Manager: Updating macro")
-                simpleRequest(
-                    f"{LOCAL_URI}/servicesNS/nobody/{self.APP_NAME}/configs/conf-macros/hibp_index",
-                    sessionKey=AUTHTOKEN,
-                    method="POST",
-                    postargs={"definition": f"index={INDEX}"},
-                    raiseAllErrors=False,
-                )
-                self.logger.info("HIBP Input Manager: Marking as configured")
-                simpleRequest(
-                    f"{LOCAL_URI}/servicesNS/nobody/{self.APP_NAME}/apps/local/hibp",
-                    sessionKey=AUTHTOKEN,
-                    method="POST",
-                    postargs={"configured": "1"},
-                    raiseAllErrors=False,
-                )
-            else:
-                self.logger.info("HIBP Input Manager: Disabling input")
+                self.logger.info("HIBP Reset: Disabling input")
                 simpleRequest(
                     f"{LOCAL_URI}/servicesNS/nobody/{self.APP_NAME}/data/inputs/hibp_domainsearch/default/disable",
                     sessionKey=AUTHTOKEN,
                     method="POST",
                     raiseAllErrors=True,
                 )
+                self.logger.info("HIBP Reset: Enabling input")
+                simpleRequest(
+                    f"{LOCAL_URI}/servicesNS/nobody/{self.APP_NAME}/data/inputs/hibp_domainsearch/default/enable",
+                    sessionKey=AUTHTOKEN,
+                    method="POST",
+                    raiseAllErrors=True,
+                )
 
-            return {"payload": "", "status": 200}
-        except Exception as e:
-            return {"payload": str(e), "status": 500}
+                return {"payload": "", "status": 200}
+            except Exception as e:
+                return {"payload": str(e), "status": 500}
+            
+        else:
+            return {"payload": "", "status": 504}
         
