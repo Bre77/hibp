b9e0370cbd436a579fabe52044d83cd53ee31f5d
Brett <brett.whynot@gmail.com>
2024-11-01 00:16:07 +0000

    Readd ReactQueryDevtools

--- src/main/webapp/shared/page.jsx
+++ src/main/webapp/shared/page.jsx
@@ -2,16 +2,16 @@ import layout from "@splunk/react-page";
 import { getUserTheme } from "@splunk/splunk-utils/themes";
 import { mixins, variables } from "@splunk/themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-//import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import styled, { createGlobalStyle } from "styled-components";
 
 const StyledContainer = styled.div`
-  ${mixins.reset("inline")};
-  display: block;
-  font-size: ${variables.fontSizeLarge};
-  line-height: 200%;
-  margin: ${variables.spacing};
+    ${mixins.reset("inline")};
+    display: block;
+    font-size: ${variables.fontSizeLarge};
+    line-height: 200%;
+    margin: ${variables.spacing};
 `;
 
 // Theme based background colour
@@ -23,31 +23,31 @@ const GlobalStyle = createGlobalStyle`
 
 // Setup the query client with defaults
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      cacheTime: Infinity,
-      staleTime: Infinity,
-      retry: false,
-      refetchOnMount: false,
-      refetchOnWindowFocus: false,
+    defaultOptions: {
+        queries: {
+            cacheTime: Infinity,
+            staleTime: Infinity,
+            retry: false,
+            refetchOnMount: false,
+            refetchOnWindowFocus: false,
+        },
     },
-  },
 });
 
 export default (child) =>
-  getUserTheme()
-    .then((theme) =>
-      layout(
-        <QueryClientProvider client={queryClient}>
-          <GlobalStyle />
-          <StyledContainer>{child}</StyledContainer>
-          <ReactQueryDevtools />
-        </QueryClientProvider>,
-        { theme },
-      ),
-    )
-    .catch((error) => {
-      const errorEl = document.createElement("span");
-      errorEl.innerHTML = error;
-      document.body.appendChild(errorEl);
-    });
+    getUserTheme()
+        .then((theme) =>
+            layout(
+                <QueryClientProvider client={queryClient}>
+                    <GlobalStyle />
+                    <StyledContainer>{child}</StyledContainer>
+                    <ReactQueryDevtools />
+                </QueryClientProvider>,
+                { theme }
+            )
+        )
+        .catch((error) => {
+            const errorEl = document.createElement("span");
+            errorEl.innerHTML = error;
+            document.body.appendChild(errorEl);
+        });
