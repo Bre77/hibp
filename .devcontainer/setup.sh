79839684aa36f56ebf6f707f6ff55daaed609519
lewis.johnston <lewis.johnston92@gmail.com>
2024-11-17 00:05:35 +0000

    update build scripts

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,5 @@
+/opt/splunk/bin/splunk cmd python3 -m pip install wheel
 /opt/splunk/bin/splunk cmd python3 -m pip install --upgrade -t src/main/resources/splunk/lib -r src/main/resources/splunk/lib/requirements.txt --no-dependencies
-yarn install
+yarn install --ignore-engines
 yarn run build
 yarn run link:app
--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,2 +1,4 @@
+#git stash
 git pull --rebase
+#git stash pop
 /opt/splunk/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd devcontainer
