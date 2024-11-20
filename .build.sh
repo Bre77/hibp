610df0d2ceb5b7778e47ba5945d851874500ef12
Brett <brett.whynot@gmail.com>
2024-11-20 05:05:31 +0000

    1.1.7

--- .build.sh
+++ .build.sh
@@ -8,9 +8,8 @@ chmod -R u-x+X stage/*
 chmod -R u=rwx,go= stage/bin/*
 mv stage hibp
 python3 -m pip install --upgrade -t hibp/lib -r hibp/lib/requirements.txt --no-dependencies
-rm -rf hibp/lib/splunklib/__pycache__
-rm -rf hibp/lib/splunklib/searchcommands/__pycache__
-rm -rf hibp/lib/splunklib/modularinput/__pycache__
+rm -rf hibp/bin/__pycache__
 rm -rf hibp/lib/*/__pycache__
+rm -rf hibp/lib/*/*/__pycache__
 tar -cpzf $OUTPUT --exclude=hibp/.* --overwrite hibp
 rm -rf hibp
